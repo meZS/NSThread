e48a5470a933ed30d41fe5589cecff113bee1d73
meZS <798506095@qq.com>
2017-07-06 10:53:52 +0800

    Initial Commit

---
+++ Nsthred/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nsthred
+//
+//  Created by wordy on 2017/7/6.
+//  Copyright © 2017年 golddatacommunications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Nsthred/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Nsthred
+//
+//  Created by wordy on 2017/7/6.
+//  Copyright © 2017年 golddatacommunications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
